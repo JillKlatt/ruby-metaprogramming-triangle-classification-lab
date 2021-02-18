0a281c36cd6714e39eb5cd42e3e2ff7bef1b438f
JillKlatt <klatt.jill@gmail.com>
2021-02-18 14:15:03 -0500

    commit tree 320ed79888dc476a1e5200ba1e237e05a62b790d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+
+  # def initialize(attributes)
+  #   attributes.each do |key, value| 
+  #     self.class.attr_accessor(key)
+  #     self.send(("#{key}="), value)
+  #   end
+  # end
+
+  def initialize(side_1, side_2, side_3)
+    @sides = [side_1, side_2, side_3]
+    @sides.sort!
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+
+  end
+  
+  def kind#(side_1, side_2, side_3)
+    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+    raise TriangleError
+    elsif @side_1 == @side_2 && @side_1 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      return :isosceles
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+      return :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "that's not a triangle you silly goose"
+    end
+
+  end
+
 end
